1e6a8bd8b5804a8c332ba4ac99fb90ed89d5c54a
Miquel Soler <miquelangelsoler@gmail.com>
2015-11-18 17:31:38 +0100

    Minor changes.

--- PMDeviceAudioAnalyzer.cpp
+++ PMDeviceAudioAnalyzer.cpp
@@ -194,7 +194,7 @@ void PMDeviceAudioAnalyzer::audioIn(float *input, int bufferSize, int nChannels)
         float currentPitchConfidence = vAubioPitches[i]->pitchConfidence;
         float modifiedSmoothingDelta=smoothingDelta*ofMap(currentPitchConfidence, 0.5, 1, 0, 1, true);
 //        bool isSilent = (currentMidiNote == 0);
-        bool isSilent = (getEnergy(i) < 0.05);
+        bool isSilent = (getEnergy(i) < 0.005);
 
         // Silence
         if (wasSilent != isSilent) // Changes in silence (ON>OFF or OFF>ON)
